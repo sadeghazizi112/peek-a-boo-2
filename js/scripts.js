3da7836acac1c95e041ea068a89eb242d2f4345d
sadegh azizi <sadeghazizi112@yahoo.com>
2018-12-21 13:57:07 -0800

    adding the extra effects from the 2.2 practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".clickable1").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#manatee-showing").fadeToggle();
+    $("#manatee-hidden").fadeToggle();
   });
 });
